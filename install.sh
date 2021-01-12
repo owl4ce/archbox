623508a334913855ba1b315b9ef932f5053e3ba4
owl4ce <dev.haxploit@gmail.com>
2021-01-12 10:41:15 +0700

    Improve

--- install.sh
+++ install.sh
@@ -1,23 +1,12 @@
 #!/usr/bin/env bash
 
-function asroot {
-    [[ $EUID -ne 0 ]] && err "Run this as root!"
-}
-
-function checkdep {
-    which $1 > /dev/null 2>&1 || err "$1 is not installed. Please install it first!"
-}
-
-function err {
-    echo "$(tput bold)$(tput setaf 1)==> Error:$(tput sgr0) $@"
-    exit 1
-}
-
-function msg {
-    echo "$(tput bold)$(tput setaf 2)==> $@ $(tput sgr0)"
-}
+asroot() { [[ $EUID -ne 0 ]] && err "Run this as root!"; }
+checkdep() { which $1 > /dev/null 2>&1 || err "$1 is not installed. Please install it first!"; }
+err() { echo "$(tput bold)$(tput setaf 1)==> Error:$(tput sgr0) $@" && exit 1; }
+msg() { echo "$(tput bold)$(tput setaf 2)==> $@ $(tput sgr0)"; }
 
 asroot
+
 case $1 in
     --uninstall)
         [[ -f /usr/local/bin/archroot ]] && source /etc/archroot.conf && clear && \
@@ -28,6 +17,7 @@ case $1 in
         else
             [[ -f /usr/local/bin/archroot ]] && \
             clear
+            /usr/local/bin/archroot -s
             while true; do
             msg "This will remove following"
             echo "/usr/local/bin/archroot"
@@ -49,29 +39,18 @@ case $1 in
     *)
         if [[ -f /usr/local/bin/archroot ]]; then
             while true; do
-            read -p $'\e[1;32m==> Archroot already installed, are you sure you want to reinstall/upgrade? \e[1;33m(without remove currently chroot env) \e[1;35m(y/n)\e[0m ' yn
+            msg "Archroot already installed"
+            read -p $'\e[1;32m==> Are you sure you want to reinstall/upgrade? \e[1;33m(except archroot.conf) \e[1;35m(y/n)\e[0m ' yn
                 case $yn in
                     [Yy]* ) clear
-                            msg "The basic configuration is as follows"
-                            cat ./archroot/archroot.conf
-                            msg "First, specify the local username, etc."
-                            echo -n "Editor of your choice (e.g: nano): "
-                            read TEXT_EDITOR
-                            checkdep $TEXT_EDITOR
-                            cp ./archroot/archroot.conf ./archroot/archroot.conf_new
-                            $TEXT_EDITOR ./archroot/archroot.conf_new || exit 1
-                            source ./archroot/archroot.conf_new
-                            clear
+                            source /etc/archroot.conf
                             msg "Installing files..."
                             # DONT CHANGE THIS
-                            mkdir -pv $INSTALL_PATH
                             install -v -D -m 755 ./archroot/archroot /usr/local/bin/archroot
-                            install -v -D -m 755 ./archroot/archroot.conf_new /etc/archroot.conf
                             install -v -D -m 755 ./archroot/copyresolv $INSTALL_PATH/copyresolv
                             install -v -D -m 755 ./archroot/command $INSTALL_PATH/command
                             install -v -D -m 755 ./archroot/archsetup $INSTALL_PATH/archsetup
-                            rm ./archroot/archroot.conf_new
-                            msg "Installation was successful"
+                            msg "Installation was successful (upgraded)"
                             echo ""
                             /usr/local/bin/archroot
                             echo ""
